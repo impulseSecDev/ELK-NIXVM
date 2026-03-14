e044cf9bae8f103a01eaec6f07d64b40ac635566
tim <timothy.harris0187@gmail.com>
2026-03-13 21:46:09 -0400

    updated to nixos 25.11 channel and updated secrets management for elk.nix

--- elk.nix
+++ elk.nix
@@ -2,12 +2,38 @@
 
 {
 
+  sops.secrets = {
+    "elasticsearch_username" = {};
+    "elasticsearch_password" = {};
+    "elastic_user" = {};
+    "elastic_password" = {};
+    "xpack_encryptedsavedobjects_encryptionkey" = {};
+    "xpack_reporting_encryptionkey" = {};
+    "xpack_security_encryptionkey" = {};
+  };
+
+  sops.templates."elastic.env" = {
+    content = ''
+      ELASTICSEARCH_USERNAME=${config.sops.placeholder."elasticsearch_username"}
+      ELASTICSEARCH_PASSWORD=${config.sops.placeholder."elasticsearch_password"}
+      ELASTIC_USERNAME=${config.sops.placeholder."elastic_user"}
+      ELASTICPASSWORD=${config.sops.placeholder."elastic_password"}
+      XPACK_ENCRYPTEDSAVEDOBJECTS_ENCRYPTIONKEY=${config.sops.placeholder."xpack_encryptedsavedobjects_encryptionkey"}
+      XPACK_REPORTING_ENCRYPTIONKEY=${config.sops.placeholder."xpack_reporting_encryptionkey"}
+      XPACK_SECURITY_ENCRYPTIONKEY=${config.sops.placeholder."xpack_security_encryptionkey"}
+   '';
+   path = "/run/secrets/elastic.env";
+   mode = "0440";
+   owner = "root";
+   group = "root";
+  };
+
   virtualisation.oci-containers = {
     backend = "docker";
     containers = {
       elasticsearch = {
         image = "docker.elastic.co/elasticsearch/elasticsearch:8.13.0";
-        environmentFiles = [ /etc/secrets/elastic.env ];
+        environmentFiles = [ config.sops.templates."elastic.env".path ];
 	volumes = [ "/var/lib/elasticsearch:/usr/share/elasticsearch/data" ];
         environment = {
           "discovery.type" = "single-node";
