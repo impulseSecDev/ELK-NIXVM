76b02623569e5e6861cd4f7de745e50ad2eb89fb
tim <timothy.harris0187@gmail.com>
2026-08-09 00:24:15 -0400

    fluent-bit: disabled logstash formatting

--- fluent-bit.nix
+++ fluent-bit.nix
@@ -255,9 +255,7 @@
           port               9200
           http_user          ${config.sops.placeholder."elastic_user"}
           http_passwd        ${config.sops.placeholder."elastic_password"}
-          logstash_format    On
-          logstash_prefix_key es_index
-          logstash_prefix    elkvm
+          logstash_format    Off
           suppress_type_name On
           buffer_size        10MB
     '';
